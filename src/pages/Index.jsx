9affe0233d75e32e5b60ab0305f52ef9a1949fad
Luis Gonzalez <lmgl887@gmail.com>
2022-11-05 23:10:52 -0600

    get customers through loader

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { useLoaderData } from 'react-router-dom'
+import { Customers } from '../services/utils/Customers';
 
-export function loader(){
+export function loader() {
 
-  return 'From loader';
+  return Customers;
 }
 
 export const Index = () => {
+
+  const customers = useLoaderData();
+  console.log(customers)
+
   return (
     <>
       <h1 className='font-black text-4xl text-blue-900'>Customers</h1>
---
+++ src/services/utils/Customers.js
@@ -0,0 +1,92 @@
+export const Customers = [
+  {
+    id: 1,
+    firstname: 'Natsumi',
+    surname: 'Minamoto',
+    phone: 102013313,
+    email: "nminamoto@yahoo.co.jp",
+    company: 'RESTAR',
+    country: 'Japan'
+  },
+  {
+    id: 2,
+    firstname: 'Haruka',
+    surname: 'Morimoto',
+    phone: 102013313,
+    email: "haruka000@gmail.com",
+    company: 'monsterlab',
+    country: 'Japan'
+  },
+  {
+    id: 3,
+    firstname: 'Hitoshi',
+    surname: 'Arakawa',
+    phone: 102013313,
+    email: "arakawah@gmail.com",
+    company: 'dotData',
+    country: 'Japan'
+  },
+  {
+    id: 4,
+    firstname: 'Tetsuya',
+    surname: 'Yamamoto',
+    phone: 102013313,
+    email: "tetsuyamamoto@yahoo.co.jp",
+    company: 'Zaiko',
+    country: 'Japan'
+  },
+  {
+    id: 5,
+    firstname: 'Todd',
+    surname: 'Maxwell',
+    phone: 102013313,
+    email: "welltodd@cisco.com",
+    company: 'Cisco',
+    country: 'United States'
+  },
+  {
+    id: 6,
+    firstname: 'Gerald',
+    surname: 'Austin',
+    phone: 102013313,
+    email: "gaustin@micron.com",
+    company: 'Micron Technology',
+    country: 'United States'
+  },
+  {
+    id: 7,
+    firstname: 'Jordan',
+    surname: 'Coleman',
+    phone: 102013313,
+    email: "colemanjj@txi.com",
+    company: 'Texas Instruments',
+    country: 'United States'
+  },
+  {
+    id: 8,
+    firstname: 'Jordan',
+    surname: 'Coleman',
+    phone: 102013313,
+    email: "colemanjj@txi.com",
+    company: 'Texas Instruments',
+    country: 'United States'
+  },
+  {
+    id: 9,
+    firstname: 'Khushi',
+    surname: 'Parekh',
+    phone: 102013313,
+    email: "pkhushi@aws.com",
+    company: 'Amazon',
+    country: 'India'
+  },
+  {
+    id: 9,
+    firstname: 'Aditi',
+    surname: 'Bhatia',
+    phone: 102013313,
+    email: "bhatiaaditi@google.com",
+    company: 'Google',
+    country: 'India'
+  },
+];
