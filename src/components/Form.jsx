aaf3ec2b0d6d35ac3e907a7e3d87125b366b217e
Luis Gonzalez <lmgl887@gmail.com>
2022-11-06 21:54:41 -0600

    add register customer form

---
+++ src/components/Form.jsx
@@ -0,0 +1,113 @@
+const Form = ({ customer }) => {
+  return (
+    <>
+      <div className="mb-4">
+        <label
+          className="text-gray-800"
+          htmlFor="firstname"
+        >First name:</label>
+        <input
+          id="firstname"
+          type="text"
+          className="mt-2 block w-full p-3 bg-gray-50"
+          placeholder="Enter customer's first name"
+          name="firstname"
+        />
+      </div>
+      <div className="mb-4">
+        <label
+          className="text-gray-800"
+          htmlFor="surname"
+        >Surname:</label>
+        <input
+          id="surname"
+          type="text"
+          className="mt-2 block w-full p-3 bg-gray-50"
+          placeholder="Enter customer's surname"
+          name="surname"
+        />
+      </div>
+      <div className="mb-4">
+        <label
+          className="text-gray-800"
+          htmlFor="company"
+        >Company:</label>
+        <input
+          id="company"
+          type="text"
+          className="mt-2 block w-full p-3 bg-gray-50"
+          placeholder="Enter customer's company"
+          name="company"
+        />
+      </div>
+      <div className="mb-4">
+        <label
+          className="text-gray-800"
+          htmlFor="company"
+        >Country:</label>
+        <select
+          id="company"
+          type="text"
+          className="mt-2 block w-full p-3 bg-gray-50"
+          placeholder="Enter customer's company"
+          name="company">
+          <option disabled selected>Select customer's country</option>
+          <option value='United States'>United States</option>
+          <option value='Japan'>Japan</option>
+          <option value='India'>India</option>
+          <option value='Malaysia'>Malaysia</option>
+          <option value='Germany'>Germany</option>
+          <option value='South Korea'>South Korea</option>
+          <option value='Israel'>Israel</option>
+          <option value='Iran'>Iran</option>
+          <option value='Hong Kong'>Hong Kong SAR</option>
+        </select>
+      </div>
+
+      <div className="mb-4">
+        <label
+          className="text-gray-800"
+          htmlFor="email"
+        >E-mail:</label>
+        <input
+          id="email"
+          type="email"
+          className="mt-2 block w-full p-3 bg-gray-50"
+          placeholder="Enter customer's email"
+          name="email"
+        />
+      </div>
+
+      <div className="mb-4">
+        <label
+          className="text-gray-800"
+          htmlFor="phone"
+        >Phone:</label>
+        <input
+          id="phone"
+          type="tel"
+          className="mt-2 block w-full p-3 bg-gray-50"
+          placeholder="Enter customer's phone number"
+          name="phone"
+        />
+      </div>
+
+      <div className="mb-4">
+        <label
+          className="text-gray-800"
+          htmlFor="notas"
+        >Notes:</label>
+        <textarea
+          as="textarea"
+          id="notes"
+          type="text"
+          className="mt-2 block w-full p-3 bg-gray-50 h-40 align-self"
+          placeholder="Enter customer's additional notes"
+          name="notas"
+        />
+      </div>
+    </>
+  )
+}
+
+export default Form
--- src/pages/NewCustomer.jsx
+++ src/pages/NewCustomer.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import Form from '../components/Form'
+
 
 const NewCustomer = () => {
+
+  const navigate = useNavigate()
+
   return (
-    <div>New Customer</div>
+    <>
+      <h1 className='font-black text-4xl text-blue-900'>New customer</h1>
+      <p className='mt-3'>Fill in all the fields to register a new customer.</p>
+      <div className='flex justify-end'>
+        <button
+          className='bg-blue-800 text-white px-3 py-1 font-bold uppercase hover:bg-blue-700 '
+          onClick={() => navigate('/')}
+        >Back</button>
+      </div>
+      <div className='bg-white shadow rounded-md  md:w-3/4 mx-auto px-5 py-10 mt-20'>
+        <form>
+        <Form/>
+        <input
+          type='submit'
+          className='mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg'
+          value='Register customer'></input>
+        </form>
+      </div>
+    </>
   )
 }
 
